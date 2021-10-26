1f44d48ecb431da7fb117ba8374ff29fad2a7c83
Gaeschlin, Quentin (qg218x) <qg218x@att.com>
2021-10-26 13:40:35 +0200

    props homework

--- src/components/GameBoard.js
+++ src/components/GameBoard.js
@@ -1,22 +1,43 @@
 import Square from "./Square";
 import style from "./GameBoard.module.css";
+import { useState } from "react";
 
 export default function GameBoard() {
+
+    const [boardState, setBoardState] = useState(["", "", "", "", "", "", "", "", ""])
+    const [player, setPlayer] = useState("X");
+
+
+    function switchMark() {
+        if (player == "X") {
+            setPlayer("O");
+        } else { setPlayer("X") }
+
+        return player;
+    }
+
+
+    function clickToSetMark(id) {
+        const newBoardState = boardState.slice();
+        newBoardState[id] = switchMark();
+        setBoardState(newBoardState);
+    }
+
     return <div>
         <div className={style.row}>
-            <Square></Square>
-            <Square></Square>
-            <Square></Square>
+            <Square id="0" board={boardState} onClick={() => clickToSetMark(0)}></Square>
+            <Square id="1" board={boardState} onClick={() => clickToSetMark(1)}></Square>
+            <Square id="2" board={boardState} onClick={() => clickToSetMark(2)}></Square>
         </div>
         <div className={style.row}>
-            <Square></Square>
-            <Square></Square>
-            <Square></Square>
+            <Square id="3" board={boardState} onClick={() => clickToSetMark(3)}></Square>
+            <Square id="4" board={boardState} onClick={() => clickToSetMark(4)}></Square>
+            <Square id="5" board={boardState} onClick={() => clickToSetMark(5)}></Square>
         </div>
         <div className={style.row}>
-            <Square></Square>
-            <Square></Square>
-            <Square></Square>
+            <Square id="6" board={boardState} onClick={() => clickToSetMark(6)}></Square>
+            <Square id="7" board={boardState} onClick={() => clickToSetMark(7)}></Square>
+            <Square id="8" board={boardState} onClick={() => clickToSetMark(8)}></Square>
         </div>
     </div>
 }
